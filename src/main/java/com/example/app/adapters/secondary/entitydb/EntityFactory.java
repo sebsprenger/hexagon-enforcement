1f3451843a19309736a0d51b3fca231f7edcfef2
Sebastian Sprenger <sebastian.sprenger@breuninger.de>
2018-12-14 20:40:43 +0100

    fix application

--- src/main/java/com/example/app/adapters/secondary/entitydb/EntityFactory.java
+++ src/main/java/com/example/app/adapters/secondary/entitydb/EntityFactory.java
@@ -1,20 +1,17 @@
 package com.example.app.adapters.secondary.entitydb;
 
-import com.example.app.adapters.primary.restapi.Harmless;
+import com.example.app.domain.model.entities.Entity;
 import com.example.app.domain.model.entities.EntityRepository;
 import com.example.app.domain.model.entities.NameRetriever;
-import com.example.app.domain.model.entities.Entity;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class EntityFactory implements EntityRepository {
 
     private NameRetriever nameRetriever;
-    private Harmless harmless;
 
-    public EntityFactory(NameRetriever nameRetriever, Harmless harmless) {
+    public EntityFactory(NameRetriever nameRetriever) {
         this.nameRetriever = nameRetriever;
-        this.harmless = harmless;
     }
 
     @Override
--- src/main/java/com/example/app/application/services/UseCase.java
+++ src/main/java/com/example/app/application/services/UseCase.java
@@ -1,8 +1,7 @@
 package com.example.app.application.services;
 
-import com.example.app.adapters.secondary.entitydb.StaticNameRetriever;
-import com.example.app.domain.model.entities.EntityRepository;
 import com.example.app.domain.model.entities.Entity;
+import com.example.app.domain.model.entities.EntityRepository;
 import com.example.app.domain.services.DomainService;
 import org.springframework.stereotype.Component;
 
@@ -11,18 +10,14 @@ public class UseCase {
 
     private DomainService service;
     private EntityRepository repo;
-    private StaticNameRetriever retriever;
 
-
-  public UseCase(DomainService service, EntityRepository repo, StaticNameRetriever retriever) {
+    public UseCase(DomainService service, EntityRepository repo) {
         this.service = service;
         this.repo = repo;
-        this.retriever = retriever;
     }
 
     public String myThing() {
         Entity entity = repo.getRandomEntity();
-        retriever.retrieveName(null);
         return service.complexOperation(entity);
     }
 
